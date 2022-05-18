06d0ec68d49fca9bff282e7e9359d9bd3345ceee
Fabian Kowatsch <f.kowatsch2000@gmail.com>
2022-05-18 16:57:44 +0200

    added calendar to model

--- app/src/main/java/com/example/futta/domain/model/Calendar.kt
+++ app/src/main/java/com/example/futta/domain/model/Calendar.kt
@@ -1,5 +1,47 @@
 package com.example.futta.domain.model
 
-class Calendar(val events: List<EventWithDates>) {
+import java.time.LocalDate
+import java.time.chrono.ChronoLocalDate
 
+class Calendar(val events: List<EventWithDates>)
+
+data class EventWithDates(
+    val event: CalendarEvent,
+    val dates: List<LocalDate>
+)
+
+fun addEventWithDates(calendar: Calendar, event: EventWithDates): Calendar {
+    val mutableEvents = calendar.events.toMutableList()
+    mutableEvents.add(event)
+    return Calendar(mutableEvents)
 }
+
+fun removeEventWithDates(calendar: Calendar, event: EventWithDates): Calendar {
+    val mutableEvents = calendar.events.toMutableList()
+    mutableEvents.removeIf { it.event.id == event.event.id }
+    return Calendar(mutableEvents)
+}
+
+fun removeDateFromEvent(calendar: Calendar, event: EventWithDates, date: LocalDate): Calendar {
+    val mutableEvents = calendar.events.toMutableList()
+    val eventIndex = mutableEvents.indexOfFirst {
+        it.event.id == event.event.id
+    }
+    val mutableDateList = event.dates.toMutableList()
+    mutableDateList.removeIf {
+        it.isEqual(ChronoLocalDate.from(date))
+    }
+    val updatedEvent: EventWithDates = event.copy(dates = mutableDateList)
+    return Calendar(mutableEvents)
+}
+
+fun updateEvent(calendar: Calendar, oldEvent: EventWithDates, newEvent: CalendarEvent): Calendar {
+    val mutableEvents = calendar.events.toMutableList()
+    val eventIndex = mutableEvents.indexOfFirst {
+        it.event.id == oldEvent.event.id
+    }
+    val updatedEvent: EventWithDates = oldEvent.copy( event = newEvent, dates = oldEvent.dates)
+    mutableEvents[eventIndex] = updatedEvent
+    return Calendar(mutableEvents)
+}
+
--- app/src/main/java/com/example/futta/domain/model/EventWithDates.kt
+++
@@ -1,8 +0,0 @@
-package com.example.futta.domain.model
-
-import java.time.LocalDate
-
-class EventWithDates(
-    val event: CalendarEvent,
-    val dates: List<LocalDate>
-)
