9288181803784bd468052167cd83e1fc9fb8526a
fabiankowatsch <f.kowatsch2000@gmail.com>
2022-06-02 21:34:02 +0200

    switched to new kotlin version

--- app/src/main/java/com/example/futta/data/database/CalendarEventDao.kt
+++ app/src/main/java/com/example/futta/data/database/CalendarEventDao.kt
@@ -1,5 +1,7 @@
 package com.example.futta.data.database
 
+import androidx.room.Dao
 
+@Dao
 interface CalendarEventDao {
 }
--- app/src/main/java/com/example/futta/feature/main/ui/MainActivity.kt
+++ app/src/main/java/com/example/futta/feature/main/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.futta
+package com.example.futta.feature.main.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.futta.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
